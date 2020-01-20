ffd2a7f8206fea9f1c3f7a5f0fde91d3d34cc6b0
benh0l <benoit.holzer8@etu.univ-lorraine.fr>
2020-01-20 15:53:24 +0100

    code fini

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,13 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.ml.fpm.FPGrowth;
+import org.apache.spark.ml.fpm.FPGrowthModel;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.*;
 import scala.Tuple2;
 
 import java.io.File;
@@ -40,8 +44,23 @@ public class Main {
 
 
         //QUESTION 1
-            //RECUPERER LES FICHIERS CF
+            //RECUPERER LES FICHIERS CF ou CP
         String path = Paths.get(Main.class.getResource("EVC-TXT/cf").getPath()).toString();
+        if(args.length == 2){
+            switch (args[1]){
+                case "CP":
+                case "cp":
+                    path = Paths.get(Main.class.getResource("EVC-TXT/cp").getPath()).toString();
+                    break;
+                case "CF":
+                case "cf":
+                    path = Paths.get(Main.class.getResource("EVC-TXT/cf").getPath()).toString();
+                    break;
+                default:
+                    path = Paths.get(Main.class.getResource("EVC-TXT/cf").getPath()).toString();
+                    break;
+            }
+        }
             //CREER RDD
         JavaRDD<String> lines = sc.textFile(path);
 
@@ -84,19 +103,47 @@ public class Main {
         countsTop10.forEach(data -> {System.out.println(data);});
 
         //PARTIE 2
-        String[] files;
-        File f = new File("EVC-TXT/cf");
-        files = f.list();
-        Dataset<Row>[] datasets = new Dataset[files.length];
+        File[] files;
+        File f = new File("src/main/resources/EVC-TXT/cf");
+        files = f.listFiles();
+        List<Row> listRow = new ArrayList();
         int i = 0;
-        for(String file: files){
+        System.out.println(files.length);
+        for(File file: files){
             //QUESTION 5
-            datasets[i] = spark.read().text(file);
-
-            //QUESTION 6
-
-            i++;
+            JavaRDD<String> rdd = sc.textFile(file.getPath());
+            rdd = rdd.flatMap(s -> Arrays.asList(s.toLowerCase().replaceAll("[^A-Za-z\\-àâäéèêëïîôöùûüÿçæœ]+"," ")
+                    .trim()
+                    .split("\\s+")).iterator())
+                    .filter(s -> !(s.isEmpty()))
+                    .subtract(stopwords); //QUESTION 6
+            listRow.add(RowFactory.create(rdd.distinct().take((int)rdd.distinct().count())));
         }
+
+        StructType schema = new StructType(new StructField[]{ new StructField(
+                "items", new ArrayType(DataTypes.StringType, true), false, Metadata.empty())
+        });
+
+        Dataset<Row> itemsDF = spark.createDataFrame(listRow, schema);
+
+        FPGrowthModel model = new FPGrowth()
+                .setItemsCol("items")
+                //QUESTION 7
+                .setMinSupport(0.8) //JOUER AVEC VALEUR
+                //QUESTION 9
+                .setMinConfidence(0.6) //JOUER AVEC VALEUR
+                .fit(itemsDF);
+
+        // Display frequent itemsets. args[0] is top-k
+        model.freqItemsets().show(Integer.parseInt(args[0]),false);
+
+        // Display generated association rules.
+        model.associationRules().show(Integer.parseInt(args[0]), false);
+
+        // transform examines the input items against all the association rules and summarize the
+        // consequents as prediction
+        model.transform(itemsDF).show(Integer.parseInt(args[0]), true);
+
 
     }
 
